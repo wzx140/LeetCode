f6d744fc20caa8bafa1cc4c85c189351a8067eb8
masterwangzx <masterwangzx@gmail.com>
2019-05-22 15:17:48 +0800

    add exercise

---
+++ src/main/java/introduce/array/FindPivotIndex.java
@@ -0,0 +1,65 @@
+package main.java.introduce.array;
+
+/**
+ * https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1144/
+ */
+public class FindPivotIndex {
+    // The solution I first came up with
+    public int pivotIndex1(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return 0;
+        }
+        // initialize left sum and right sum
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 1; i < nums.length; i++) {
+            rightSum += nums[i];
+        }
+
+        if (leftSum == rightSum) {
+            return 0;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            leftSum += nums[i - 1];
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Simplified solution
+     */
+    public int pivotIndex2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int leftSum = 0;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == sum - leftSum - nums[i]) {
+                    return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FindPivotIndex findPivotIndex = new FindPivotIndex();
+        int[] nums = { -1, -1, 0, 1, 1, 0 };
+        System.out.println(findPivotIndex.pivotIndex1(nums));
+        System.out.println(findPivotIndex.pivotIndex2(nums));
+    }
+}
