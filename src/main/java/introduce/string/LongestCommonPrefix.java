4bcd7a2ca83d97924f9709c192d01bd86f2c5f55
masterwangzx <masterwangzx@gmail.com>
2019-07-03 15:06:27 +0800

    add exercise

---
+++ src/main/java/introduce/string/LongestCommonPrefix.java
@@ -0,0 +1,41 @@
+package main.java.introduce.string;
+
+// https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1162/
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        } else if (strs.length == 1) {
+            return strs[0];
+        }
+        //  find the Minimum value
+        int minLength = Integer.MAX_VALUE;
+        for (String str : strs) {
+            if (str.length() < minLength) {
+                minLength = str.length();
+            }
+        }
+        int length = minLength;
+
+        char[] res = new char[length];
+        for (int i = 0; i < length; i++) {
+            boolean coincidence = true;
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[0].charAt(i) != strs[j].charAt(i)) {
+                    coincidence = false;
+                }
+            }
+            if (!coincidence) {
+                break;
+            }
+            res[i] = strs[0].charAt(i);
+        }
+        return String.valueOf(res).trim();
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix soluton = new LongestCommonPrefix();
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(soluton.longestCommonPrefix(strs));
+    }
+}
