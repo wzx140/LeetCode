c9c37e900fd7764063d4d5619392dcfbc04f6097
masterwangzx <masterwangzx@gmail.com>
2019-07-05 10:19:36 +0800

    add exercise

---
+++ src/main/java/introduce/TwoPointerTechnique/RemoveElement.java
@@ -0,0 +1,34 @@
+package main.java.introduce.TwoPointerTechnique;
+
+// https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1151/
+public class RemoveElement {
+
+    //    fast and slow pointer, this function has two many copy(line10)
+    public int removeElement1(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+
+        return slow;
+    }
+
+    // since the question does not require arrays to be ordered
+    public int removeElement2(int[] nums, int val) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            if (nums[i] == val) {
+                nums[i] = nums[j];
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return nums.length - j - 1;
+    }
+
+}
---
+++ src/main/java/introduce/TwoPointerTechnique/TwoSumIISorted.java
@@ -0,0 +1,51 @@
+package main.java.introduce.TwoPointerTechnique;
+
+// https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1153/
+public class TwoSumIISorted {
+
+    //    too slow, Not used that the array is sorted
+    public int[] twoSum1(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    int[] res = {i + 1, j + 1};
+                    return res;
+                }
+            }
+        }
+        return null;
+    }
+
+    //    also slow
+    public int[] twoSum2(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                int add = numbers[i] + numbers[j];
+                if (add == target) {
+                    int[] res = {i + 1, j + 1};
+                    return res;
+                } else if (add > target) {
+                    break;
+                }
+            }
+        }
+        return null;
+    }
+
+    //    use two pointer point to head and tail then move to middle
+    public int[] twoSum3(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+        while (i < j) {
+            int add = numbers[i] + numbers[j];
+            if (add > target) {
+                j -= 1;
+            } else if (add < target) {
+                i += 1;
+            } else if (add == target) {
+                return new int[]{i + 1, j + 1};
+            }
+        }
+        return null;
+    }
+}
