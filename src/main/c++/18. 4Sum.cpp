71ae5f8a09c2135239af0d098e6a587511f5ea7a
masterwangzx <masterwangzx@gmail.com>
2019-09-24 10:24:33 +0800

    new exercise

---
+++ src/main/c++/18. 4Sum.cpp
@@ -0,0 +1,65 @@
+﻿#include "Header.h"
+
+#include <algorithm>
+
+using std::sort;
+
+/**
+ * https://leetcode.com/problems/4sum/
+ */
+
+ /**
+  * 每次先确定两个数，再用双指针，类似3sum中的解法
+  * time:O(n^3)
+  * space:O(1)
+  */
+vector<vector<int>> Sum4::fourSum1(vector<int>& nums, int target) {
+	int length = nums.size();
+	vector<vector<int>> res;
+
+	sort(nums.begin(), nums.end());
+
+	for (int i = 0; i < length - 3; i++) {
+
+		// 防止重复
+		if (i > 0 && nums.at(i) == nums.at(i - 1)) {
+			continue;
+		}
+
+		for (int j = i + 1; j < length - 2; j++) {
+
+			// 防止重复
+			if (j > i + 1 && nums.at(j) == nums.at(j - 1)) {
+				continue;
+			}
+
+			int left = j + 1;
+			int right = length - 1;
+			while (left < right) {
+
+				// 防止重复
+				if (left > j + 1 && nums[left] == nums[left - 1]) {
+					left++;
+					continue;
+				}
+				if (right < length - 1 && nums[right] == nums[right + 1]) {
+					right--;
+					continue;
+				}
+
+				int sum = nums.at(i) + nums.at(j) + nums.at(left) + nums.at(right);
+				if (sum == target) {
+					res.push_back({ nums.at(i) , nums.at(j) , nums.at(left) , nums.at(right) });
+					left++;
+					right--;
+				} else if (sum < target) {
+					left++;
+				} else if (sum > target) {
+					right--;
+				}
+			}
+		}
+	}
+
+	return res;
+}
---
+++ src/test/CTest/18. 4Sum Test.cpp
@@ -0,0 +1,76 @@
+﻿#include "pch.h"
+#include "../../main/c++/Header.h"
+
+#include <unordered_set>
+
+using std::unordered_set;
+
+class Sum4Test : public ::testing::Test {
+protected:
+	void SetUp() override {
+		source.emplace_back(-1);
+		source.emplace_back(0);
+		source.emplace_back(1);
+		source.emplace_back(0);
+		source.emplace_back(-2);
+		source.emplace_back(2);
+
+		res1.insert(-1);
+		res1.insert(0);
+		res1.insert(0);
+		res1.insert(1);
+
+		res2.insert(-2);
+		res2.insert(-1);
+		res2.insert(1);
+		res2.insert(2);
+
+		res2.insert(-2);
+		res2.insert(0);
+		res2.insert(0);
+		res2.insert(2);
+	}
+
+	void TearDown() override {}
+
+	vector<int> source;
+	unordered_set<int> res1;
+	unordered_set<int> res2;
+	unordered_set<int> res3;
+};
+
+
+
+TEST_F(Sum4Test, fourSum1Test) {
+	Sum4 solution;
+	auto solutions = solution.fourSum1(source,0);
+	unordered_set<int> solution1;
+	unordered_set<int> solution2;
+	unordered_set<int> solution3;
+	solution1.insert(solutions.at(0).at(0));
+	solution1.insert(solutions.at(0).at(1));
+	solution1.insert(solutions.at(0).at(2));
+	solution1.insert(solutions.at(0).at(3));
+	solution2.insert(solutions.at(1).at(0));
+	solution2.insert(solutions.at(1).at(1));
+	solution2.insert(solutions.at(1).at(2));
+	solution2.insert(solutions.at(1).at(3));
+	solution3.insert(solutions.at(2).at(0));
+	solution3.insert(solutions.at(2).at(1));
+	solution3.insert(solutions.at(2).at(2));
+	solution3.insert(solutions.at(2).at(3));
+
+	if (res1 == solution1&&res2==solution2) {
+		EXPECT_TRUE(res3 == solution3);
+	} else if(res1==solution1 && res2 == solution3){
+		EXPECT_TRUE(res3 == solution2);
+	} else if (res1 == solution2 && res2 == solution1) {
+		EXPECT_TRUE(res3 == solution3);
+	} else if (res1 == solution2 && res2 == solution3) {
+		EXPECT_TRUE(res3 == solution1);
+	} else if (res1 == solution3 && res2 == solution1) {
+		EXPECT_TRUE(res3 == solution2);
+	} else if (res1 == solution3 && res2 == solution2) {
+		EXPECT_TRUE(res3 == solution1);
+	}
+}
