62f61958943c23933fafa16a5fa7589ad00dab62
wzx <masterwangzx@gmail.com>
2020-12-07 10:10:51 +0800

    new exercise

--- src/main/java/com/wzx/leetcode/No772BasicCalculator3.java
+++ src/main/java/com/wzx/leetcode/No772BasicCalculator3.java
@@ -33,36 +33,39 @@ public class No772BasicCalculator3 {
       char ch = s.charAt(i);
       // 数值
       if (Character.isDigit(ch)) curNum = curNum * 10 + (ch - '0');
-      // 符号或者最后一位触发更新
-      if ((!Character.isDigit(ch) && !Character.isWhitespace(ch)) || i == s.length() - 1 || ch == ')') {
-        if (curOp == '*' || curOp == '/') {
-          // 乘除直接运算
-          int lastNum = state.stack.pollFirst();
-          state.stack.addFirst(cal(lastNum, curNum, curOp));
-        } else if (curOp == '+' || curOp == '-') {
-          // 加减缓存起来
-          state.stack.addFirst(cal(0, curNum, curOp));
-        }
-        // 更新
-        curOp = ch;
-        curNum = 0;
-      }
-      // 缓存并重置现场
+      // 左括号触发缓存并重置现场
       if (ch == '(') {
         state.op = curOp;
         stack.addFirst(state);
         state = new State();
         curOp = '+';
       }
-      if (ch == ')') {
-        // 计算括号内的值
-        curNum = state.stack.stream().mapToInt(Integer::intValue).sum();
-        // 取出缓存的现场中
-        state = stack.pollFirst();
-        curOp = state.op;
+      // 符号或者最后一位或右括号触发更新计算
+      if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == ')' || i == s.length() - 1) {
+        // 计算操作
+        if (curOp == '*' || curOp == '/') {
+          // 乘除直接运算
+          int lastNum = state.stack.pollFirst();
+          state.stack.addFirst(cal(lastNum, curNum, curOp));
+        }
+        if (curOp == '+' || curOp == '-') {
+          // 加减缓存起来
+          state.stack.addFirst(cal(0, curNum, curOp));
+        }
+        // 更新操作
+        if (ch == ')') {
+          // 计算括号内的值
+          curNum = state.stack.stream().mapToInt(Integer::intValue).sum();
+          // 取出缓存的现场中
+          state = stack.pollFirst();
+          curOp = state.op;
+        } else {
+          curOp = ch;
+          curNum = 0;
+        }
       }
     }
-    return state.stack.stream().mapToInt(Integer::intValue).sum();
+    return cal(state.stack.stream().mapToInt(Integer::intValue).sum(), curNum, curOp);
   }
 
   private int cal(int num1, int num2, char op) {
@@ -81,6 +84,7 @@ public class No772BasicCalculator3 {
         res = num1 / num2;
         break;
       default:
+        res = num1 + num2;
         break;
     }
     return res;
--- src/test/java/com/wzx/leetcode/No772BasicCalculator3Test.java
+++ src/test/java/com/wzx/leetcode/No772BasicCalculator3Test.java
@@ -10,6 +10,6 @@ public class No772BasicCalculator3Test {
     No772BasicCalculator3 solution = new No772BasicCalculator3();
     assertEquals(2, solution.calculate("1 + 1"));
     assertEquals(4, solution.calculate(" 6-4 / 2"));
-    assertEquals(4, solution.calculate("2*(5+5*2)/3+(6/2+8)"));
+    assertEquals(21, solution.calculate("2*(5+5*2)/3+(6/2+8)"));
   }
 }
